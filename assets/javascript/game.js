e50e8ef5a0a29e2522587f6b7f44f3eed29a35a5
Owomugisha <brinnetbirungi@yahoo.com>
2018-11-18 19:18:27 -0500

    letter that the computer is going to choose from, set var to zero, computer choose a random letter, accept the user input and store in the variable, compare userchoice vs computerchoice, generated a new letter and assign to compute choice, update guessesLeft, displaying the new value of guessesLeft to the DOM, updating your guesses so far with an empty value, displaying the new value of your guesses so far, reduce guessesLeft by 1, updating your huesses so far with user value , displaying the new value of your guesses so far, update guessesLeft, displaying the new value of guessesLeft to the Dom, increases the value of losses, displaying the new losses, updating your guesses so far with an empty value, displaying the new value of your guesses so far

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,60 @@
-var x = 1;
-var y = 5;
-var total;
-x + y = total;
-console.log(total);
+//letters that the computer is going to choose from 
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var userChoice, computerChoice;
+// set var to zero 
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessedLetter = "";
+
+// 1. computer choose a random letter 
+computerChoice = computerChoices[Math.floor(Math.random() * 26)];
+//console.log(computerChoice);
+// 2. accept the user input and store in the variable 
+document.onkeyup = function (event) {
+    userChoice = event.key;
+    //  3. compare userChoice VS computerChoice using if statement 
+    if (userChoice == computerChoice) {
+        wins++;
+        $("#wins").text(wins);
+        //generate a new letter and assign to computer choice
+        computerChoice = computerChoices[Math.floor(Math.random() * 26)];
+        // update guessesleft
+        guessesLeft = 9;
+        //displaying the new value of guessesleft to the DOM
+        $("#guessesLeft").text(guessesLeft);
+        //updating your guesses so far with an empty value
+        guessedLetter = "";
+        //displaying the new value of your guesses so far 
+        $("#guessesSoFar").text(guessedLetter);
+    }
+    else { //  4. reduce guessesLeft by 1
+        if (guessesLeft > 1) {
+            guessesLeft--;
+            $("#guessesLeft").text(guessesLeft);
+            //updating your guesses so far with user value
+            guessedLetter = guessedLetter + userChoice + ", ";
+            //displaying the new value of your guesses so far 
+            $("#guessesSoFar").text(guessedLetter);
+        } else {
+            // update guessesleft
+            guessesLeft = 9;
+            //displaying the new value of guessesleft to the DOM
+            $("#guessesLeft").text(guessesLeft);
+            //increase the value of Losses
+            losses++;
+            //displaying the new losses
+            $("#losses").text(losses);
+            computerChoice = computerChoices[Math.floor(Math.random() * 26)];
+            console.log(computerChoice);
+            //updating your guesses so far with an empty value
+            guessedLetter = "";
+            //displaying the new value of your guesses so far 
+            $("#guessesSoFar").text(guessedLetter);
+
+        }
+    }
+
+}
+
+
